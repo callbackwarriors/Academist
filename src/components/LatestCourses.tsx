a7f570fae7755ee1821bde2a5ecd139b7724150c
Probir Ghosh <probirghosh@outlook.com>
2021-09-22 15:59:26 +0600

    latest courses making ongoing

--- src/components/LatestCourses.tsx
+++ src/components/LatestCourses.tsx
@@ -1,42 +1,43 @@
 const LatestCourses = () => {
   return (
-    <div className="latest__courses">
+    <section className="latest__courses">
       <div className="latest__courses--top">
         <h1 className="latest__courses--top--heading">Our Latest Courses</h1>
         <button className="latest__courses--top--button">View All</button>
       </div>
-      <div className="card__container">
-        <div className="card__container--section">
-          <div className="card__container--section--imgBX">
-            <img src="https://i.ibb.co/9V1Xq8s/crd-img.webp" alt="" />
-          </div>
-          <div className="card__container--section--content">
-              <h5>Learn Python Programming Beginner</h5>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor tempora beatae quod ipsa molestias iste voluptates, asperiores eos excepturi quae?</p>
-          </div>
-        </div>
+      <div className="card">
+        <div className="card__container--main">
+          <div className="card__container--main--section">
+            <div className="p-4 md:w-1/3">
+              <div className="card__container--main--section--contextBX">
+                <img src="https://i.ibb.co/9V1Xq8s/crd-img.webp" alt="" />
+                <div className="p-6">
+                  <h5>Learn Python Programming Beginner</h5>
+                </div>
+              </div>
+            </div>
 
-        <div className="card__container--section">
-          <div className="card__container--section--imgBX">
-            <img src="https://i.ibb.co/9V1Xq8s/crd-img.webp" alt="" />
-          </div>
-          <div className="card__container--section--content">
-              <h5>Learn Python Programming Beginner</h5>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor tempora beatae quod ipsa molestias iste voluptates, asperiores eos excepturi quae?</p>
-          </div>
-        </div>
+            <div className="p-4 md:w-1/3">
+              <div className="card__container--main--section--contextBX">
+                <img src="https://i.ibb.co/9V1Xq8s/crd-img.webp" alt="" />
+                <div className="p-6">
+                  <h5>Learn Python Programming Beginner</h5>
+                </div>
+              </div>
+            </div>
 
-        <div className="card__container--section">
-          <div className="card__container--section--imgBX">
-            <img src="https://i.ibb.co/9V1Xq8s/crd-img.webp" alt="" />
-          </div>
-          <div className="card__container--section--content">
-              <h5>Learn Python Programming Beginner</h5>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor tempora beatae quod ipsa molestias iste voluptates, asperiores eos excepturi quae?</p>
+            <div className="p-4 md:w-1/3">
+              <div className="card__container--main--section--contextBX">
+                <img src="https://i.ibb.co/9V1Xq8s/crd-img.webp" alt="" />
+                <div className="p-6">
+                  <h5>Learn Python Programming Beginner</h5>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
