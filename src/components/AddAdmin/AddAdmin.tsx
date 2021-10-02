dd19c53e7c519eebf8a7d65613b7afc5b5811554
callbackwarriors <91022130+callbackwarriors@users.noreply.github.com>
2021-10-02 13:12:17 +0600

    fix add admin

--- src/components/AddAdmin/AddAdmin.tsx
+++ src/components/AddAdmin/AddAdmin.tsx
@@ -15,66 +15,66 @@ const AddAdmin = () => {
           <div className="lg:col-span-3">
             <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
               <div className="md:col-span-5">
-                <label for="full_name">Full Name</label>
+                <label>Full Name</label>
                 <input type="text" name="full_name" id="full_name" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" />
               </div>
 
               <div className="md:col-span-5">
-                <label for="email">Email Address</label>
+                <label>Email Address</label>
                 <input type="text" name="email" id="email" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" placeholder="email@domain.com" />
               </div>
 
               <div className="md:col-span-3">
-                <label for="address">Address / Street</label>
+                <label>Address / Street</label>
                 <input type="text" name="address" id="address" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" placeholder="" />
               </div>
 
               <div className="md:col-span-2">
-                <label for="city">City</label>
+                <label>City</label>
                 <input type="text" name="city" id="city" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" placeholder="" />
               </div>
 
               <div className="md:col-span-2">
-                <label for="country">Country / region</label>
+                <label>Country / region</label>
                 <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
                   <input name="country" id="country" placeholder="Country" className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent" value="" />
-                  <button tabindex="-1" className="cursor-pointer outline-none focus:outline-none transition-all text-gray-300 hover:text-red-600">
+                  <button className="cursor-pointer outline-none focus:outline-none transition-all text-gray-300 hover:text-red-600">
                     <svg className="w-4 h-4 mx-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                       <line x1="18" y1="6" x2="6" y2="18"></line>
                       <line x1="6" y1="6" x2="18" y2="18"></line>
                     </svg>
                   </button>
-                  <button tabindex="-1" for="show_more" className="cursor-pointer outline-none focus:outline-none border-l border-gray-200 transition-all text-gray-300 hover:text-blue-600">
+                  <button className="cursor-pointer outline-none focus:outline-none border-l border-gray-200 transition-all text-gray-300 hover:text-blue-600">
                     <svg className="w-4 h-4 mx-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="18 15 12 9 6 15"></polyline></svg>
                   </button>
                 </div>
               </div>
 
               <div className="md:col-span-2">
-                <label for="state">State / province</label>
+                <label>State / province</label>
                 <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
                   <input name="state" id="state" placeholder="State" className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent" value="" />
-                  <button tabindex="-1" className="cursor-pointer outline-none focus:outline-none transition-all text-gray-300 hover:text-red-600">
+                  <button className="cursor-pointer outline-none focus:outline-none transition-all text-gray-300 hover:text-red-600">
                     <svg className="w-4 h-4 mx-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                       <line x1="18" y1="6" x2="6" y2="18"></line>
                       <line x1="6" y1="6" x2="18" y2="18"></line>
                     </svg>
                   </button>
-                  <button tabindex="-1" for="show_more" className="cursor-pointer outline-none focus:outline-none border-l border-gray-200 transition-all text-gray-300 hover:text-blue-600">
+                  <button className="cursor-pointer outline-none focus:outline-none border-l border-gray-200 transition-all text-gray-300 hover:text-blue-600">
                     <svg className="w-4 h-4 mx-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="18 15 12 9 6 15"></polyline></svg>
                   </button>
                 </div>
               </div>
 
               <div className="md:col-span-1">
-                <label for="zipcode">Zipcode</label>
+                <label>Zipcode</label>
                 <input type="text" name="zipcode" id="zipcode" className="transition-all flex items-center h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="" value="" />
               </div>
 
               <div className="md:col-span-5">
                 <div className="inline-flex items-center">
                   <input type="checkbox" name="billing_same" id="billing_same" className="form-checkbox" />
-                  <label for="billing_same" className="ml-2">My billing address is different than above.</label>
+                  <label className="ml-2">My billing address is different than above.</label>
                 </div>
               </div>
 
