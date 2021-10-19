c1004c0c15e8ec0050ca38e09d12197d874bd648
callbackwarriors <91022130+callbackwarriors@users.noreply.github.com>
2021-10-19 13:19:37 +0600

    added some fixngs

--- src/components/AddNewCourse/AddNewCourse.tsx
+++ src/components/AddNewCourse/AddNewCourse.tsx
@@ -2,7 +2,6 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import { useFormState } from 'react-hook-form';
 import { useMutate } from 'restful-react';
 
 
@@ -19,7 +18,7 @@ const AddNewCourse = () => {
    
     const {mutate: uplodeImage} = useMutate({
           verb: "POST",
-          path: "api/postCourse/img-uplode"
+          path: "api/postcourses/img-uplode"
     });
     const handleImage = (event:any) => {
          setSelectImage(event.target.files[0])
@@ -45,7 +44,7 @@ const AddNewCourse = () => {
         e.preventDefault();
 
         try {
-            const { data } = await axios.post("/api/postCourse/postCourse", {
+            const { data } = await axios.post("/api/postcourses/postCourse", {
                 title,
                 slug,
                 videoUrl,
@@ -133,7 +132,7 @@ const AddNewCourse = () => {
                                             <div className="flex flex-col items-center justify-center cursor-pointer pt-7">
                                                 <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-gray-400 group-hover:text-gray-600"
                                                     fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                                         d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                                                 </svg>
                                                 <p className="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">
