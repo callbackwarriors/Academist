2f6a61d6401282400eb87fd3f49855161bb4a7d9
saymon shoab <mdsaymonshoab@gmail.com>
2021-10-21 01:19:29 +0600

    instractor detail page

--- src/pages/instractors/index.js
+++ src/pages/instractors/index.js
@@ -1,9 +1,14 @@
+import InstractorCard from 'components/InstractorCard/InstractorCard';
 import Footer from 'components/utilities/Footer';
 import Header from 'components/utilities/Header';
+import User from 'models/User';
 import React from 'react';
 import { AiFillEye, AiFillVideoCamera, AiOutlineHeart, AiOutlineStar } from 'react-icons/ai';
 import { BsFillGrid3X2GapFill, BsFillGrid3X3GapFill, BsSearch } from 'react-icons/bs';
-const Instractors = () => {
+import db from 'utils/db';
+const Instractors = (props) => {
+    console.log('user info',props)
+    const {user} = props;
     return (
         <div className="sectionAllCourses">
             <Header />
@@ -174,96 +179,14 @@ const Instractors = () => {
                                 </div>
                             </div>
 
-                            <div className="flex flex-wrap page-content__body">
-                                <div className="p-4 transition duration-100 ease-in-out transform xl:w-1/3 md:w-1/2 hover:scale-105">
-                                    <div className="p-6 bg-gray-100 rounded-lg">
-                                        <div className="relative">
-                                            <img className="object-cover object-center w-full h-40 mb-6 rounded" src="https://i.ibb.co/9V1Xq8s/crd-img.webp" alt="" />
-                                            <AiOutlineHeart className="absolute text-2xl top-3 right-3" />
-                                        </div>
+            <div className="flex flex-wrap page-content__body">
+                               
+                  {
+                      user.map((ur)=> <InstractorCard key={ur._id} ur={ur} /> )
+                  }
+            </div>
 
-                                        <div className="flex justify-between gap-3 pb-3 mb-3 border-b border-gray-300">
-                                            <div className="flex items-center justify-center gap-1">
-                                                <AiOutlineStar className="text-2xl text-yellow-400" /> 4.5
-                                            </div>
-                                            <div className="flex items-center justify-center gap-1">
-                                                <AiFillEye className="text-2xl text-red-400" /> 2300
-                                            </div>
-                                            <div className="flex items-center justify-center gap-1">
-                                                <AiFillVideoCamera className="text-2xl text-green-400" /> 15
-                                            </div>
-                                        </div>
-                                        <h4 className="mb-2 text-2xl">Colosseum Roma</h4>
-                                        <p className="text-base leading-relaxed">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-                                    </div>
-                                </div>
 
-                                <div className="p-4 transition duration-100 ease-in-out transform xl:w-1/3 md:w-1/2 hover:scale-105">
-                                    <div className="p-6 bg-gray-100 rounded-lg">
-                                        <div className="relative">
-                                            <img className="object-cover object-center w-full h-40 mb-6 rounded" src="https://i.ibb.co/9V1Xq8s/crd-img.webp" alt="" />
-                                            <AiOutlineHeart className="absolute text-2xl top-3 right-3" />
-                                        </div>
-                                        <div className="flex justify-between gap-3 pb-3 mb-3 border-b border-gray-300">
-                                            <div className="flex items-center justify-center gap-1">
-                                                <AiOutlineStar className="text-2xl text-yellow-400" /> 4.5
-                                            </div>
-                                            <div className="flex items-center justify-center gap-1">
-                                                <AiFillEye className="text-2xl text-red-400" /> 2300
-                                            </div>
-                                            <div className="flex items-center justify-center gap-1">
-                                                <AiFillVideoCamera className="text-2xl text-green-400" /> 15
-                                            </div>
-                                        </div>
-                                        <h4 className="mb-2 text-2xl">Colosseum Roma</h4>
-                                        <p className="text-base leading-relaxed">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-                                    </div>
-                                </div>
-
-                                <div className="p-4 transition duration-100 ease-in-out transform xl:w-1/3 md:w-1/2 hover:scale-105">
-                                    <div className="p-6 bg-gray-100 rounded-lg">
-                                        <div className="relative">
-                                            <img className="object-cover object-center w-full h-40 mb-6 rounded" src="https://i.ibb.co/9V1Xq8s/crd-img.webp" alt="" />
-                                            <AiOutlineHeart className="absolute text-2xl top-3 right-3" />
-                                        </div>
-                                        <div className="flex justify-between gap-3 pb-3 mb-3 border-b border-gray-300">
-                                            <div className="flex items-center justify-center gap-1">
-                                                <AiOutlineStar className="text-2xl text-yellow-400" /> 4.5
-                                            </div>
-                                            <div className="flex items-center justify-center gap-1">
-                                                <AiFillEye className="text-2xl text-red-400" /> 2300
-                                            </div>
-                                            <div className="flex items-center justify-center gap-1">
-                                                <AiFillVideoCamera className="text-2xl text-green-400" /> 15
-                                            </div>
-                                        </div>
-                                        <h4 className="mb-2 text-2xl">Colosseum Roma</h4>
-                                        <p className="text-base leading-relaxed">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-                                    </div>
-                                </div>
-
-                                <div className="p-4 transition duration-100 ease-in-out transform xl:w-1/3 md:w-1/2 hover:scale-105">
-                                    <div className="p-6 bg-gray-100 rounded-lg">
-                                        <div className="relative">
-                                            <img className="object-cover object-center w-full h-40 mb-6 rounded" src="https://i.ibb.co/9V1Xq8s/crd-img.webp" alt="" />
-                                            <AiOutlineHeart className="absolute text-2xl top-3 right-3" />
-                                        </div>
-                                        <div className="flex justify-between gap-3 pb-3 mb-3 border-b border-gray-300">
-                                            <div className="flex items-center justify-center gap-1">
-                                                <AiOutlineStar className="text-2xl text-yellow-400" /> 4.5
-                                            </div>
-                                            <div className="flex items-center justify-center gap-1">
-                                                <AiFillEye className="text-2xl text-red-400" /> 2300
-                                            </div>
-                                            <div className="flex items-center justify-center gap-1">
-                                                <AiFillVideoCamera className="text-2xl text-green-400" /> 15
-                                            </div>
-                                        </div>
-                                        <h4 className="mb-2 text-2xl">Colosseum Roma</h4>
-                                        <p className="text-base leading-relaxed">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -274,3 +197,15 @@ const Instractors = () => {
 };
 
 export default Instractors;
+
+
+export async function getServerSideProps() {
+    await db.connect();
+    const user = await User.find({}).lean();
+    await db.disconnect();
+    return {
+      props: {
+        user: user.map(db.convertDocToObj),
+      },
+    };
+  }
