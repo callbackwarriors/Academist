d80a0d6a2f2ae1227890e3f57ec2177eaf0b7761
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-10-23 10:39:18 +0600

    test

--- src/models/Courses.js
+++ src/models/Courses.js
@@ -15,28 +15,7 @@ const CoursesSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const Courses = mongoose.models.Courses || mongoose.model("Test", CoursesSchema);
+const Courses =
+  mongoose.models.Courses || mongoose.model("Test", CoursesSchema);
 
 export default Courses;
-
-
-// const mongoose = require("mongoose");
-// const CoursesSchema = new mongoose.Schema(
-//   {
-//     name: { type: String, required: true, unique: true }, // unique: true
-//     slug: { type: String, required: true, unique: true }, // unique: true
-//     category: { type: String, required: true },
-//     image: { type: String, required: true },
-//     price: { type: Number, required: true },
-//     brand: { type: String, required: true },
-//     rating: { type: Number, required: true, default: 0 },
-//     numReviews: { type: Number, required: true, default: 0 },
-//     countInStock: { type: Number, required: true, default: 0 },
-//     description: { type: String, required: true },
-//   },
-//   { timestamps: true }
-// );
-
-// const Courses = mongoose.models.Courses || mongoose.model("Courses", CoursesSchema);
-
-// export default Courses;
