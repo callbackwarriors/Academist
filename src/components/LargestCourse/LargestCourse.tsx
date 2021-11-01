c2f7d049113e68bedc0808ac09844c9e01ace9dd
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-11-01 16:17:53 +0600

    prichard page compilit

--- src/components/LargestCourse/LargestCourse.tsx
+++ src/components/LargestCourse/LargestCourse.tsx
@@ -1,14 +1,14 @@
-import c1 from 'assets/images/c1.jpg';
+import Link from 'next/link'
 import Image from 'next/image';
 import { BsBook, BsHeart } from 'react-icons/bs';
 import { HiOutlineUserGroup } from 'react-icons/hi';
 
 const LargestCourse = ({ course }: any) => {
-    const { img, title, price } = course;
+    const { img, title, price, slug } = course;
     return (
 
         <>
-            <div className="largestCourse__cardSection">
+            <div className="mb-6 md:mb-0 largestCourse__cardSection">
                 <div className="largestCourse__cardSection--imgSection">
                     <div className="inline-flex p-2 align-middle bg-white rounded-full shadow">
                         <Image width="150px" height="150px" src={img} alt="" />
@@ -19,7 +19,11 @@ const LargestCourse = ({ course }: any) => {
                         <p> <BsHeart /></p>
                     </div>
                     <h5>${price}</h5>
-                    <h6>{title}</h6>
+                    <Link href={`/courses/${slug}`}>
+                        <a>
+                            <h6>{title}</h6>
+                        </a>
+                    </Link>
                     <div className="flex justify-between details">
                         <div className="flex leftSide">
                             <h6><BsBook></BsBook></h6>
