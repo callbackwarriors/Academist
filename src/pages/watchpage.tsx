66c32c735e0aa163435c1281763ce2f8454252bb
Foysal Hossain <fimran2011@gmail.com>
2021-11-02 10:22:09 +0600

    test

---
+++ src/pages/watchpage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const watchpage = () => {
+  return (
+    <div className="container p-4 flex">
+      <div className="w-full p-4 lg:w-2/3">This is course watchpage</div>
+      <div>
+        <div>Lesson</div>
+        <div>Lesson</div>
+        <div>Lesson</div>
+      </div>
+    </div>
+  );
+};
+
+export default watchpage;
