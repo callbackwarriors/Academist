049f6a175bcaa62c35e1492e870248dccade1574
callbackwarriors <91022130+callbackwarriors@users.noreply.github.com>
2021-11-04 00:02:02 +0600

    categories

--- src/components/Category/CategoryData.tsx
+++ src/components/Category/CategoryData.tsx
@@ -1,75 +1,39 @@
 
 const CategoryData = [
-
     {
         id:1,
-        title:"Web Development",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, voluptate!",
-        logo:"view Courses",
-        courseDetails:"view Courses"
+        title:"React",
+        description:"React. js is an open-source JavaScript library that is used for building user interfaces specifically for single-page applications.",
+        logo:"https://res.cloudinary.com/academist/image/upload/v1635921798/navc7gva5abgh7bk9jmr.jpg",
+        link:"/search?query=react"
       },
       {
         id:2,
-        title:"Life Style",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, voluptate!",
-        logo:"",
-        courseDetails:"view Courses"
+        title:"NextJS",
+        description:"Next. js is a React framework that enables several extra features, including server-side rendering and generating static websites.",
+        logo:"https://res.cloudinary.com/academist/image/upload/v1635922191/md7oi2axqvpltszdtghm.jpg",
+        link:"/search?query=nextjs"
       },
       {
         id:3,
-        title:"Photography",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, voluptate!",
-        logo:"",
-        courseDetails:"view Courses"
+        title:"MEARN",
+        description:"MERN stands for MongoDB, Express, React, Node, after the four key technologies that make up the stack.",
+        logo:"https://res.cloudinary.com/academist/image/upload/v1635922555/zrliqmcdztwe1k7xvlyn.jpg",
+        link:"/search?query=mearn"
       },
       {
         id:4,
-        title:"Health & Fitness",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, voluptate!",
-        logo:"",
-        courseDetails:"view Courses"
+        title:"NodeJS",
+        description:" Node. js is primarily used for non-blocking, event-driven servers, due to its single-threaded nature",
+        logo:"https://res.cloudinary.com/academist/image/upload/v1635922322/lhxxzkm5qtq5vyncckjr.jpg",
+        link: "/search?query=nodejs",
       },
       {
         id:5,
-        title:"Accounting",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, voluptate!",
-        logo:"",
-        courseDetails:"view Courses"
-      },
-      {
-        id:6,
-        title:"Gov. Exam",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, voluptate!",
-        logo:"",
-        courseDetails:"view Courses"
-      },
-      {
-        id:7,
-        title:"Office Productivity",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, voluptate!",
-        logo:"",
-        courseDetails:"view Courses"
-      },
-      {
-        id:8,
-        title:"Web Designing",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, voluptate!",
-        logo:"",
-        courseDetails:"view Courses"
-      },
-      {
-        id:9,
-        title:"Development",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, voluptate!",
-        logo:"",
-        courseDetails:"view Courses"
-      },
-      {
-        id:10,
-        title:"Photography",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, voluptate!",
-        logo:"",
-        courseDetails:"view Courses"
+        title:"MongoDB",
+        description:"MongoDB is a document-oriented database which stores data in JSON-like documents with dynamic schema.",
+        logo:"https://res.cloudinary.com/academist/image/upload/v1635922860/d3ane2iwvzte3kzpc0fa.jpg",
+        link:"/search?query=mongodb",
       }
 
 ]
